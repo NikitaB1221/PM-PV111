49ff22c45b9ef3cfbba3d8b78d5554b205d86638
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2024-01-19 18:39:21 +0200

    ght

--- ConsoleAppCS/Program.cs
+++ ConsoleAppCS/Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World(fv)!");
-Console.WriteLine("Hello, World(six)!");
+Console.WriteLine("Hello, World(ght)!");
